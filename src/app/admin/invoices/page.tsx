f17a0a3f902ad554aaac41e5ea8005d30fcb281c
vadym-loik <loikfit@gmail.com>
2024-01-11 14:53:54 +0100

    Add authentication and authorization logic to invoices page

--- src/app/admin/invoices/page.tsx
+++ src/app/admin/invoices/page.tsx
@@ -3,6 +3,8 @@ import './invoices.scss';
 
 import { getPartnerData } from '@/db/schema';
 import { Metadata } from 'next';
+import { auth, currentUser } from '@clerk/nextjs/server';
+// import { redirect } from 'next/navigation';
 
 export const metadata: Metadata = {
   title: 'Hopways Facture',
@@ -11,6 +13,20 @@ export const revalidate = 0;
 
 const Home = async () => {
   const data = await getPartnerData();
+  const user = await currentUser();
+  const { userId } = auth();
+  const isNotAdmin =
+    !user ||
+    !user.privateMetadata ||
+    !('role' in user.privateMetadata) ||
+    ('role' in user.privateMetadata &&
+      typeof user.privateMetadata.role === 'string' &&
+      !user.privateMetadata.role.includes('admin'));
+
+  if (!userId || isNotAdmin) {
+    // redirect('/');
+    return <div>Sorry you do not have acces</div>;
+  }
 
   return (
     <>
